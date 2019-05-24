d45d0f755c2067acd39075548e1181f132ba7f3e
d.o.IT <contact@dmmo-online.de>
2019-05-24 14:36:08 +0200

    Add project files.

---
+++ ConvertAspnetboilerplateXml2Resx/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Resources;
+using System.Xml;
+
+namespace ConvertAspnetboilerplateXml2Resx
+{
+    internal class Program
+    {
+        private const string _aspnetboilerblateXmlLocalizationFile = "erp.xml";
+        private const string _outputResxFile = "Erp.resx";
+        private static void Main(string[] args)
+        {
+
+            XmlTextReader reader = new XmlTextReader(_aspnetboilerblateXmlLocalizationFile);
+
+            ResXResourceWriter writer = new ResXResourceWriter(_outputResxFile);
+
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element && reader.Name == "text")
+                {
+                    var name = reader.GetAttribute("name");
+
+                    Debug.WriteLine(string.Format("{0} - value {1} - inner: {2}", name, reader.GetAttribute("value"), reader.Value));
+
+                    writer.AddResource(name, reader.Value.Trim() != "" ? reader.Value : reader.GetAttribute("value"));
+                }
+            }
+
+            writer.Generate();
+            writer.Close();
+        }
+    }
+}
---
+++ ConvertAspnetboilerplateXml2Resx/readme.md
@@ -0,0 +1,19 @@
+﻿# Convert ASP.NET Boilerplate XML Localization to resx 
+
+Convert any existing .xml localization file to resx
+
+### Prerequisites
+
+. Net Framework 4.7.2
+
+### Doing
+
+Change the const string to your .xml
+
+private const string _aspnetboilerblateXmlLocalizationFile = "erp.xml";
+private const string _outputResxFile = "Erp.resx";
+
+
+## License
+
+This project is licensed under the MIT License
